1b260e6cba39d4b908e9f86f76f66b059c88251d
Projjal Sengupta <projjalsengupto@gmail.com>
2019-11-30 16:04:02 +0530

    Added SpiralOrderMatrixOne and SpiralOrderMatrixTwo

--- src/Amazon/Array/SpiralOrderMatrixTwo.java
+++ src/Amazon/Array/SpiralOrderMatrixTwo.java
@@ -1,7 +1,6 @@
 package Amazon.Array;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class SpiralOrderMatrixTwo {
---
+++ src/Amazon/Array/WaveArray.java
@@ -0,0 +1,37 @@
+package Amazon.Array;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class WaveArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<Integer> A = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            A.add(scanner.nextInt());
+        }
+        System.out.println(wave(A));
+    }
+
+    private static ArrayList<Integer> wave(ArrayList<Integer> A) {
+        Collections.sort(A);
+        for (int i = 0; i < A.size(); i += 2) {
+            if (i > 0 && A.get(i - 1) > A.get(i)) {
+                Collections.swap(A, i, i - 1);
+            }
+            if (i < A.size() - 1 && A.get(i) < A.get(i + 1)) {
+                Collections.swap(A, i, i + 1);
+            }
+        }
+        return A;
+    }
+
+    private static void print(ArrayList<Integer> A) {
+        for (Integer integer : A) {
+            System.out.println(integer + " ");
+        }
+        System.out.println();
+    }
+}
