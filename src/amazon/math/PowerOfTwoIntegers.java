ef579debb7ca29607d8331ab80784126abadaeaa
Projjal Sengupta <projjalsengupto@gmail.com>
2019-12-01 02:07:58 +0530

    Added Power of two integers

---
+++ src/amazon/math/PowerOfTwoIntegers.java
@@ -0,0 +1,24 @@
+package amazon.math;
+
+import java.util.Scanner;
+
+public class PowerOfTwoIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        System.out.println(isPower(A));
+    }
+
+    private static int isPower(int A) {
+        if (A == 1) {
+            return 1;
+        }
+        for (int i = 1; i <= Math.sqrt(A); i++) {
+            int P = (int) (Math.log(A) / Math.log(i));
+            if (Math.pow(i, P) == A) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
