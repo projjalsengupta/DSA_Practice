63e41e682f80124e5170fadf3712d48153d0216c
Projjal Sengupta <projjalsengupto@gmail.com>
2019-12-01 05:33:52 +0530

    Added ReverseBits and SingleNumber

---
+++ src/amazon/bitmanipulation/ReverseBits.java
@@ -0,0 +1,21 @@
+package amazon.bitmanipulation;
+
+import java.util.Scanner;
+
+public class ReverseBits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        System.out.println(reverse(n));
+    }
+
+    private static long reverse(long a) {
+        long res = 0;
+        int i = 31;
+        while (a > 0) {
+            res += (a % 2) * Math.pow(2, i--);
+            a = a / 2;
+        }
+        return res;
+    }
+}
