e2dd6ccdfc8641c7a43d4387ebdf7587f900c663
Projjal Sengupta <projjalsengupto@gmail.com>
2019-12-04 02:40:52 +0530

    Added Tree Codes

---
+++ src/amazon/tree/InorderPreorderPostOrder.java
@@ -0,0 +1,101 @@
+package amazon.tree;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class InorderPreorderPostOrder {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+            left = null;
+            right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.println("Inorder");
+        print(inorder(root));
+        System.out.println("Preorder");
+        print(preorder(root));
+        System.out.println("Postorder");
+        print(postorder(root));
+    }
+
+    private static ArrayList<Integer> inorder(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        TreeNode temp = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while (temp != null || !stack.isEmpty()) {
+            while (temp != null) {
+                stack.push(temp);
+                temp = temp.left;
+            }
+            temp = stack.pop();
+            res.add(temp.val);
+            temp = temp.right;
+        }
+        return res;
+    }
+
+    private static ArrayList<Integer> preorder(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        TreeNode temp = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while (temp != null || !stack.isEmpty()) {
+            while (temp != null) {
+                res.add(temp.val);
+                stack.push(temp);
+                temp = temp.left;
+            }
+            temp = stack.pop();
+            temp = temp.right;
+        }
+        return res;
+    }
+
+    private static ArrayList<Integer> postorder(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        TreeNode temp = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while (temp != null || !stack.isEmpty()) {
+            while (temp != null) {
+                stack.push(temp);
+                stack.push(temp);
+                temp = temp.left;
+            }
+            temp = stack.pop();
+            if (!stack.isEmpty() && stack.peek() == temp) {
+                temp = temp.right;
+            } else {
+                res.add(temp.val);
+                temp = null;
+            }
+        }
+        return res;
+    }
+
+    private static void print(ArrayList<Integer> a) {
+        for (Integer i : a) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/amazon/tree/LeastCommonAncestor.java
@@ -0,0 +1,48 @@
+package amazon.tree;
+
+public class LeastCommonAncestor {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+            left = null;
+            right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.println(lca(root, 11, 12));
+    }
+
+    private static int lca(TreeNode A, int B, int C) {
+        TreeNode treeNode = lcaNode(A, B, C);
+        if (treeNode != null) {
+            return treeNode.val;
+        }
+        return -1;
+    }
+
+    private static TreeNode lcaNode(TreeNode A, int B, int C) {
+        if (A == null) {
+            return null;
+        }
+        if (A.val == B || A.val == C) {
+            return A;
+        }
+        TreeNode node1 = lcaNode(A.left, B, C);
+        TreeNode node2 = lcaNode(A.right, B, C);
+        if (node1 != null && node2 != null) {
+            return A;
+        }
+        return (node1 == null) ? node2 : node1;
+    }
+}
